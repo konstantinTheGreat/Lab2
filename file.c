4914886db8ab7e4cdb408056f681eb409b7d5cf4
konstantinTheGreat <124327537+konstantinTheGreat@users.noreply.github.com>
2023-11-16 15:09:36 +0200

    Add files via upload

--- file.c
+++ file.c
@@ -3,33 +3,37 @@
 #include <sys/types.h>
 #include <stdio.h>
 int main() { 
-    pid_t pid;
+    pid_t pid; 
     for (int i = 0; i<2; i++) {
-        pid = fork();
-
-        if (pid == 0) {
-            if (i == 0) {
-                pid = fork();
-                printf("Child process %d (PID: %d) with parent (PPID: %d)\n", i + 1, getpid(), getppid());
-                for (int j = 0 ; j < 3 ; j++) {
-                    pid = fork();
-                    if (pid == 0){
-                        printf("Child process %d (PID: %d) with parent (PPID: %d)\n", j + i + 1, getpid(), getppid());
-                        if (j + i + 1 == 6) {
-                            int fd = open("./output.txt",O_WRONLY | O_CREAT, 0777);
-                            dprintf(fd, "pid: %d\n", getpid());
-                            dprintf(fd, "execl(/bin/ls,ls, -1, NULL)");
-                            execl("/bin/ls","ls", "-1", NULL); 
-                            close(fd);
-                        } 
-                    } else {
-                        waitpid (pid, NULL, 0);
-                    }
-                }        
-            }
+        pid = fork(); 
+	
+        if (pid == 0) { 
             printf("Child process %d (PID: %d) with parent (PPID: %d)\n", i + 1, getpid(), getppid());
+            if (i == 0) {  
+                pid = fork(); 
+                if (pid == 0) { 
+                	printf("Child process %d (PID: %d) with parent (PPID: %d)\n", 3, getpid(), getppid());
+                	for (int j = 4; j < 7; j++) { 
+                    		pid = fork();
+                    		if (pid == 0){ 
+                        		printf("Child process %d (PID: %d) with parent (PPID: %d)\n", j, getpid(), getppid());
+                        		if (j == 5) { 
+                            			int fd = open("./output.txt",O_WRONLY | O_CREAT, 0777);
+                            			dprintf(fd, "pid: %d\n", getpid());
+                            			dprintf(fd, "execl(/bin/ls,ls, -1, NULL)");
+                            			execl("/bin/ls","ls", "-1", NULL); 
+                            			close(fd);
+                        		} 
+                		} else {
+                			wait(NULL);
+                		}  
+                	} 
+            	} else {
+            		wait(NULL);
+            	} 
+            }
         } else {
-            waitpid (pid, NULL, 0);
+            wait(NULL);
         }
     }
     return 0;
